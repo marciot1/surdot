5e9d194936bd2f7f249caeca08afcf2c7a79a0f3
marciot1 <donascimentotargino@gmail.com>
2023-07-09 23:01:05 -0300

    surdot

---
+++ surdot/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+const SurdT = (props: any) => {
+  return(
+    <>
+      <div>
+        <h1><a href='https://www.google.com/search?q=surdo&rlz=1C1CHBD_pt-PTBR1065BR1065&oq=surdo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIKCAEQABixAxiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIGCAcQRRg80gEIMTg3NmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8'>SurdoT</a></h1>
+      </div>
+      <ul>
+        <li><a href="https://github.com/">Git</a></li>
+      </ul>
+
+    </>
+  )
+}
+function App(props: any) {
+  return (
+    <>
+      <SurdT/>
+    </>
+  )
+}
+
+export default App
